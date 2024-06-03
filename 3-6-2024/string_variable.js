666a09dff9e6ac9879fa8a5c831ea0ce3b2ec4be
Mohammad7mam <127339065+Mohammad7mam@users.noreply.github.com>
2024-06-03 05:13:07 +0300

    Add files via upload

---
+++ 3-6-2024/string_variable.js
@@ -0,0 +1,117 @@
+ 0// 1-  Declare a variable called "name" and assign it the value "John". 
+ /******* Start Your Code *********/
+  var name = "john";
+
+
+ /******* End Your Code ********* */
+
+
+// 2- Declare a constant called "PI" and assign it the value 3.14.
+ /******* Start Your Code *********/
+  const PI = 3.14;
+
+
+ /******* End Your Code ********* */
+
+// 3- Declare a variable called "age" and assign it your age.
+ /******* Start Your Code *********/
+
+  var age = 26;
+
+
+ /******* End Your Code ********* */
+
+// 4- Declare a variable called "isStudent" and assign it a boolean value.
+ /******* Start Your Code *********/
+  var isStudent = true ;
+
+
+ /******* End Your Code ********* */
+
+// 5- Create a string variable called "message" and assign it a message of your choice.
+ /******* Start Your Code *********/
+var message = 'gg';
+
+
+ /******* End Your Code ********* */
+
+// 6- Use template literals to create a string that includes the variables "name" and "age".
+ /******* Start Your Code *********/
+ const name = "Mohammad"
+ const age = "26"
+ const sentence = "My name is " + name + " and I am " + age + " years old.";
+
+ /******* End Your Code ********* */
+
+// 7- Use the string method "length" to find the length of the string "message".
+ /******* Start Your Code *********/
+ let message = "lorem lLorem";
+ let messageLength = message.length;//11 
+
+ /******* End Your Code ********* */
+
+// 8- Use the string method "toUpperCase()" to convert the string "message" to uppercase. 
+/******* Start Your Code *********/
+  let messageUppercase = message.toUpperCase();
+
+
+ /******* End Your Code ********* */
+
+// 9- Use the string method "toLowerCase()" to convert the string "message" to lowercase.
+ /******* Start Your Code *********/
+ let messageLowercase = message.toLowerCase();
+  
+
+
+ /******* End Your Code ********* */
+
+// 10- Use the string method "indexOf()" to find the position of the word "JavaScript" in the string "I love learning JavaScript".
+ /******* Start Your Code *********/
+  let message = "I love learning JavaScript"
+  let index = message.indexOf(JavaScript);
+
+
+ /******* End Your Code ********* */
+
+// 11- Use the string method "slice()" to extract a portion of the string "message".
+ /******* Start Your Code *********/
+  let message = " lorem lLorem ipsum dolor sit amet consectetur "
+  let message2 = message.slice(20,25);
+
+
+ /******* End Your Code ********* */
+
+// 12- Declare a variable called "num1" and assign it a number.
+ /******* Start Your Code *********/
+  var num1 = 217
+
+
+
+ /******* End Your Code ********* */
+
+// 13- Declare a variable called "num2" and assign it another number.
+ /******* Start Your Code *********/
+  var num2 = 187
+
+
+ /******* End Your Code ********* */
+
+// 14- Use the arithmetic operator "+" to add num1 and num2.
+ /******* Start Your Code *********/
+   var num3 = num1+num2
+
+
+ /******* End Your Code ********* */
+
+// 15- Use the arithmetic operator "-" to subtract num1 from num2.
+ /******* Start Your Code *********/
+  var num4 = num1-num2
+
+
+ /******* End Your Code ********* */
+ 
+
+
+
+
+
